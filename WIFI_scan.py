5ba45339658b755dbb4553c42aad1e362ce22205
Shubham Telang <76528092+ShubhamTelang@users.noreply.github.com>
2021-05-13 21:06:57 +0530

    Update WIFI_scan.py

--- WIFI_scan.py
+++ WIFI_scan.py
@@ -4,3 +4,4 @@ net=network.WLAN(network.STA_IF)             #Load station interface
 net.active(True)                             #Activate wifi
 net.scan()                                   #Scan wifi
 net.connect('Your ssid','Your password')     #connect esp to wifi
+ap.ifconfig()
